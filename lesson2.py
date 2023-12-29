66c06a53175d0b326f6a31f30b4a5d51b0efa026
allnc <meibrohimov@gmail.com>
2023-12-30 00:45:50 +0500

    add new file

--- lesson2.py
+++ lesson2.py
@@ -1,3 +1,12 @@
+
+"""
+Simmetrik kalitli shifrlash:
+Simmetrik kalitli shifrlashda ma'lumotlar bir xil kalit bilan kodlanadi va dekodlanadi.
+ Bu shifrlashning eng oson yo'li, ammo xavfsizroq. Qabul qiluvchiga shifrni ochish uchun
+kalit kerak, shuning uchun kalitlarni uzatishning xavfsiz usuli kerak. Kalitga ega bo'lgan
+ har bir kishi o'rtadagi ma'lumotlarni o'qiy oladi.
+""" # noqa
+
 from cryptography.fernet import Fernet
 
 # biz quyidagi qatorni shifrlaymiz. # noqa
@@ -8,7 +17,7 @@ shifrlash va shifrni ochish uchun kalit yaratish
 Yaratish uchun fernetdan foydalanishingiz mumkin
 kalit yoki tasodifiy kalit generatoridan foydalaning
 bu yerda men kalit yaratish uchun fernetdan foydalanmoqdaman
-"""
+""" # noqa
 
 key = Fernet.generate_key()
 
@@ -19,7 +28,7 @@ fernet = Fernet(key)
 keyin Fernet sinf misolidan foydalaning
 qatorni shifrlash kerak
 shifrlashdan oldin bayt qatoriga kodlangan bo'lishi kerak
-"""
+""" # noqa
 encMessage = fernet.encrypt(message.encode())
 
 print("original string: ", message)
@@ -31,7 +40,7 @@ Fernet namunasi,
 bu satrni shifrlash uchun ishlatilgan
 kodlangan bayt satri shifrni ochish usuli bilan qaytariladi,
 shuning uchun uni dekodlash usullari bilan satrga dekodlang
-"""
+"""  # noqa
 decMessage = fernet.decrypt(encMessage).decode()
 
 print("decrypted string: ", decMessage)
